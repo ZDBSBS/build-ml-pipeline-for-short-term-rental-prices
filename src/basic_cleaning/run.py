bd41af9baf19de0e3554021c18af34037fe084da
Zied Besbes <zied.besbes@gmail.com>
2026-04-20 13:23:12 +0000

    Finalize pipeline and set best hyperparameters

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+"""
+Download from W&B the raw dataset and apply some basic data cleaning,
+exporting the result to a new artifact
+"""
+import argparse
+import logging
+import pandas as pd
+import wandb
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    # Start a W&B run
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(vars(args))
+
+    logger.info("Downloading input artifact")
+
+    # Download input artifact (and register lineage)
+    artifact = run.use_artifact(args.input_artifact)
+    artifact_local_path = artifact.file()
+
+    # Load dataset
+    df = pd.read_csv(artifact_local_path)
+
+    logger.info("Applying basic data cleaning")
+
+    # Remove price outliers
+    idx = df["price"].between(args.min_price, args.max_price)
+    df = df[idx].copy()
+
+    # Convert last_review to datetime
+    df["last_review"] = pd.to_datetime(df["last_review"])
+
+    # Save cleaned data
+    cleaned_filename = "clean_sample.csv"
+    df.to_csv(cleaned_filename, index=False)
+
+    logger.info("Uploading cleaned dataset as artifact")
+
+    # Create and log output artifact
+    output_artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    output_artifact.add_file(cleaned_filename)
+    run.log_artifact(output_artifact)
+
+    run.finish()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
+
+    parser.add_argument(
+        "--input_artifact",
+        type=str,
+        help="Name of the input dataset artifact to clean",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--output_artifact",
+        type=str,
+        help="Name of the output cleaned dataset artifact",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--output_type",
+        type=str,
+        help="Type of the output artifact (e.g. cleaned_data)",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--output_description",
+        type=str,
+        help="Description of the output artifact",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--min_price",
+        type=float,
+        help="Minimum allowed price (EDA-based)",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--max_price",
+        type=float,
+        help="Maximum allowed price (EDA-based)",
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    go(args)
